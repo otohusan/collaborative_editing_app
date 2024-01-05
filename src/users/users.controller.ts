341e611959849c0775a92311b4e251eabd003d6b
otohusan <tamioguitar@gmail.com>
2024-01-05 20:31:31 +0900

    feat: コントローラとサービスを連携

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { User } from 'interfaces/user.interfaces';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   findAll(): string {
-    return 'This action returns super cats';
+    return this.usersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `Hi Jon ${id}`;
+  }
+
+  @Post()
+  async create(@Body() user: User): Promise<User[]> {
+    return this.usersService.create(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../../interfaces/user.interfaces';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [];
+
+  findAll() {
+    return 'return all';
+  }
+
+  create(user: User) {
+    this.users.push(user);
+    return this.users;
+  }
+}
