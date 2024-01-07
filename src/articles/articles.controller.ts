0f08c6e3ae0d438171c38a83af139f1770fdfe7f
otohusan <tamioguitar@gmail.com>
2024-01-07 13:39:24 +0900

    feat: articlesを全権取得する関数を追加

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { Articles } from 'interfaces/articles.interfaces';
 import { ArticlesService } from './articles.service';
 
@@ -6,10 +6,20 @@ import { ArticlesService } from './articles.service';
 export class ArticlesController {
   constructor(private readonly articleService: ArticlesService) {}
 
+  @Get()
+  async getArticles(): Promise<Articles[]> {
+    return this.articleService.getArticles();
+  }
+
   @Get('user/:id')
-  async getArticles(@Param('id') id: string): Promise<Articles[]> {
+  async getUserArticles(@Param('id') id: string): Promise<Articles[]> {
     // paramはstringで取得されるのでnumberに変換する必要がある
     const user_id = parseInt(id, 10);
     return this.articleService.getUserArticles(user_id);
+  }
+
+  @Post()
+  async createArticles(@Body() createArticleDto: Articles): Promise<Articles> {
+    return this.articleService.createArticle(createArticleDto);
   }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { Articles } from 'interfaces/articles.interfaces';
 import { UsersService } from 'src/users/users.service';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class ArticlesService {
-  constructor(private readonly usersService: UsersService) {}
+  private readonly prisma: PrismaClient;
+
+  constructor(private readonly usersService: UsersService) {
+    this.prisma = new PrismaClient();
+  }
+
+  async getArticles(): Promise<Articles[]> {
+    const articles = await this.prisma.articles.findMany();
+    return articles;
+  }
+
+  async createArticle(data: {
+    text: string;
+    userId: number;
+  }): Promise<Articles> {
+    return await this.prisma.articles.create({
+      data: {
+        text: data.text,
+        userId: data.userId,
+      },
+    });
+  }
 
   async getUserArticles(user_id: number): Promise<Articles[]> {
     const user = this.usersService.findOne(user_id);
