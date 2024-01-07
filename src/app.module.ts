8032d3b5165be5a11787541ee4dd485ed67046cd
otohusan <tamioguitar@gmail.com>
2024-01-07 12:34:23 +0900

    chore: articleのサービスとコントローラを作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ArticlesController } from './articles/articles.controller';
+import { ArticlesService } from './articles/articles.service';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ArticlesController],
+  providers: [AppService, ArticlesService],
 })
 export class AppModule {}
